7a130a09242b336ce99d27605d35fc93c1a5e5ec
BenMemi <ben.memisevic@mycelium.ventures>
2022-10-03 15:41:31 +0200

    changes

--- up.sh
+++ up.sh
@@ -4,9 +4,10 @@ sed -n '5,8p' .env > terraform/start_script.sh
 echo 'sudo wget -c https://go.dev/dl/go1.19.1.linux-amd64.tar.gz' >> terraform/start_script.sh
 echo 'sudo tar -C /usr/local -xvzf go1.19.1.linux-amd64.tar.gz' >> terraform/start_script.sh
 echo 'export  PATH=$PATH:/usr/local/go/bin' >> terraform/start_script.sh
+
 echo 'git clone https://github.com/BenMemi/Repository-Activity-Tracker-.git' >> terraform/start_script.sh
 echo 'cd Repository-Activity-Tracker-' >> terraform/start_script.sh
-echo 'go build' >> terraform/start_script.sh
+echo 'sudo go build' >> terraform/start_script.sh
 echo 'echo "Running tracker"' >> terraform/start_script.sh
 echo 'chmod 777 terraform/start_script.sh' >> terraform/start_script.sh
 echo './tracker' >> terraform/start_script.sh
@@ -14,3 +15,6 @@ echo './tracker' >> terraform/start_script.sh
 
 
 
+
+sudo visudo
+replace line with + 
